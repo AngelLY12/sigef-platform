d7fc77c0ca25506a15b351d82a9685f3db5dce68
Angel <lopezyanezangell@gmail.com>
2026-06-09 10:48:55 -0600

    feat: add support for external links in anchor component

--- frontend/school-management/src/app/shared/components/ui/anchor/anchor.component.ts
+++ frontend/school-management/src/app/shared/components/ui/anchor/anchor.component.ts
@@ -7,10 +7,12 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './anchor.component.html',
-  styleUrl: './anchor.component.scss'
+  styleUrl: './anchor.component.scss',
 })
 export class AnchorComponent {
   @Input() link: string = '';
   @Input() anchorMessage: string = '';
-
+  @Input() external = false;
+  @Input() target: '_self' | '_blank' = '_self';
+  @Input() rel = 'noopener noreferrer';
 }
