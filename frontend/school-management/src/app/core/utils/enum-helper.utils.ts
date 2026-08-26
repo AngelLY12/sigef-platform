87193e1cdef31bb7536fd7150633792865e8db0e
Angel <lopezyanezangell@gmail.com>
2026-08-26 13:44:47 -0600

    feat: add enum options helper

--- frontend/school-management/src/app/core/utils/enum-helper.utils.ts
+++ frontend/school-management/src/app/core/utils/enum-helper.utils.ts
@@ -1,6 +1,18 @@
+import { SelectOption } from "../../shared/components/form/controls/select/select-option.config.model";
+
 export function enumToOptions<T extends Record<string, string>>(enumObj: T) {
   return Object.values(enumObj).map(value => ({
     label: value,
     value
+  }));
+}
+
+export function enumToOptionsWithLabel<T extends Record<string, string>>(
+  enumObj: T,
+  labels: Record<T[keyof T], string>,
+): SelectOption[] {
+  return (Object.values(enumObj) as T[keyof T][]).map((value) => ({
+    label: labels[value],
+    value,
   }));
 }
