8a48a30618473bae9e0647d6a3971e4836047f02
Angel <lopezyanezangell@gmail.com>
2026-08-26 13:44:08 -0600

    feat: add http params helper

--- frontend/school-management/src/app/core/utils/params-helper.utils.ts
+++ frontend/school-management/src/app/core/utils/params-helper.utils.ts
@@ -1,3 +1,6 @@
+import { HttpParams } from "@angular/common/http";
+import { cleanObject } from "../helpers";
+
 export function createParams<T>(
   base: T,
   overrides: Partial<T> = {}
@@ -6,4 +9,13 @@ export function createParams<T>(
     ...base,
     ...overrides
   };
+}
+
+export function createHttpParams<T extends object>(filters: T): HttpParams {
+  return new HttpParams({
+    fromObject: Object.fromEntries(
+      Object.entries(cleanObject(filters))
+        .map(([key, value]) => [key, String(value)])
+    )
+  });
 }
